aef1e3d48926ab1904826929e146e78ebc511c86
matheussmends <matheussmends@gmail.com>
2025-03-24 18:24:29 -0300

    Atualizando código e banco de dados

--- app/database/db.py
+++ app/database/db.py
@@ -1,22 +1,22 @@
 from sqlalchemy import create_engine
-# create_engine() cria a conexão com o banco SQLite
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Define o nome do banco de dados SQLite
-DATABASE_URL = "sqlite:///./pedidos.db"
+DATABASE_URL = "sqlite:///./pedidos.db"  # Caminho para o banco de dados SQLite
 
-# Cria a engine do banco de dados
+# Cria a engine do banco de dados (SQLite)
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
 # Cria a sessão para interagir com o banco
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 # Base para as classes do SQLAlchemy
-# Base usado para criar os modelos do banco
 Base = declarative_base()
 
 # Dependência para obter a sessão do banco de dados
 def get_db():
-    db = SessionLocal()
+    db = SessionLocal()  # Cria a sessão do banco de dados
     try:
-        yield db
+        yield db  # Retorna a sessão aberta para ser usada nas rotas
     finally:
-        db.close()
+        db.close()  # Fecha a sessão após o uso
--- app/main.py
+++ app/main.py
@@ -1,29 +1,18 @@
 from fastapi import FastAPI
 from app.database.db import engine, Base
-
-app = FastAPI()
-
-# Cria as tabelas no banco de dados
-Base.metadata.create_all(bind=engine)
-
-@app.get("/")
-def home():
-    return {"mensagem": "API de Monitoramento de Pedidos de Delivery"}
-
-# IMPORTANDO ROTAS
-
-from fastapi import FastAPI
-from app.database.db import engine, Base
 from app.routes import pedido_routes
+from app.models import pedido  # Importação do modelo de pedido
 
-app = FastAPI()
-
-# Criando as tabelas no banco de dados
+# Criação das tabelas no banco de dados (executa isso uma vez)
 Base.metadata.create_all(bind=engine)
+
+# Inicializando a aplicação FastAPI
+app = FastAPI()
 
 # Incluindo as rotas de pedidos
 app.include_router(pedido_routes.router)
 
+# Rota de exemplo
 @app.get("/")
 def home():
     return {"mensagem": "API de Monitoramento de Pedidos de Delivery"}
